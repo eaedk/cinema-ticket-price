f24576946cf908abd9f28d13888bd2de3bad1a05
E K <emmanuelkoupoh@gmail.com>
2022-07-17 21:10:06 +0000

    pricing commented

--- code/src/pricing.py
+++ code/src/pricing.py
@@ -17,24 +17,24 @@ def price(dayOfWeek, isOldOrYoung=False, isNight=False):
 
     """
 
-    weekendDays = [
-        "fri",
-        "sat",
-        "sun",
-    ]
-    normal_price = 30
-    weekendDiscount = -5
-    oldYoungDiscount = -7
-    morningDiscount = -10
+    # weekendDays = [
+    #     "fri",
+    #     "sat",
+    #     "sun",
+    # ]
+    # normal_price = 30
+    # weekendDiscount = -5
+    # oldYoungDiscount = -7
+    # morningDiscount = -10
 
-    price = normal_price
+    # price = normal_price
 
-    # week day
-    if dayOfWeek.lower() in weekendDays:
-        price += weekendDiscount
-    if isNight:
-        price += morningDiscount
-    if isOldOrYoung:
-        price += oldYoungDiscount
+    # # week day
+    # if dayOfWeek.lower() in weekendDays:
+    #     price += weekendDiscount
+    # if isNight:
+    #     price += morningDiscount
+    # if isOldOrYoung:
+    #     price += oldYoungDiscount
 
     return price
