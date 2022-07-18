56c49350d5f2dff643f2162d256e1597a42a86a5
Emmanuel KOUPOH <emmanuel.koupoh@baamtu.com>
2022-07-18 20:59:35 +0000

    update with import

--- tests/test_jupyter_nb.py
+++ tests/test_jupyter_nb.py
@@ -8,3 +8,8 @@ def test_func_value():
 
 def test_class_Number() :
     assert isinstance(notebook.Number(10), int)
+
+def test_pandas() :
+    # x = pd.DataFrame({"i": [0, 1, 2]})
+    print(notebook.Number(10).df)
+    assert 1 == 1
